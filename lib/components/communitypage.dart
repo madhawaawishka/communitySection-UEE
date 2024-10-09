99a89d03c1d407e9e2e55d43c8c280d34518d6f4
madhawaawishka99 <madhawaawishka@gmail.com>
2024-10-09 12:03:48 +0530

    adding the swipe left right to the edit and delete post function

--- lib/components/communitypage.dart
+++ lib/components/communitypage.dart
@@ -2,6 +2,7 @@ import 'package:community/components/wall_posts.dart';
 import 'package:community/helper/helper_method.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart'; // Import fluttertoast package for showing toast messages
 import 'new_post.dart'; // Import the new post page
 
 class CommunityPage extends StatefulWidget {
@@ -12,6 +13,48 @@ class CommunityPage extends StatefulWidget {
 }
 
 class _CommunityPageState extends State<CommunityPage> {
+  final String currentUserEmail = "madhawaawishka@gmail.com"; // Hardcoded user email
+
+  // Function to delete a post
+  void deletePost(String postId) {
+    FirebaseFirestore.instance.collection('User Posts').doc(postId).delete();
+    Fluttertoast.showToast(msg: "Post deleted successfully.");
+  }
+
+  // Function to edit a post
+  void editPost(BuildContext context, String postId, String currentMessage) {
+    TextEditingController editController = TextEditingController(text: currentMessage);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Edit Post"),
+          content: TextField(
+            controller: editController,
+            maxLines: 5,
+            decoration: const InputDecoration(
+              hintText: "Edit your post",
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                FirebaseFirestore.instance.collection('User Posts').doc(postId).update({
+                  'Message': editController.text
+                });
+                Navigator.pop(context);
+                Fluttertoast.showToast(msg: "Post updated successfully.");
+              },
+              child: const Text("Save"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,15 +64,14 @@ class _CommunityPageState extends State<CommunityPage> {
           style: TextStyle(
             color: Colors.black,
             fontWeight: FontWeight.bold,
-          ), // Set the text color to black
+          ),
         ),
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        centerTitle: true, // Center the title
+        centerTitle: true,
         actions: [
           IconButton(
-            icon: const Icon(Icons.add_circle, color: Colors.black), // Plus icon in black color
+            icon: const Icon(Icons.add_circle, color: Colors.black),
             onPressed: () {
-              // Navigate to new_post.dart
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const NewPostPage()),
@@ -38,28 +80,66 @@ class _CommunityPageState extends State<CommunityPage> {
           ),
         ],
       ),
-      backgroundColor: Colors.grey[200], // Change this color to any you like
+      backgroundColor: Colors.grey[200],
       body: Center(
         child: Column(
           children: [
             Expanded(
               child: StreamBuilder(
-                stream: FirebaseFirestore.instance
-                    .collection("User Posts")
-                    .orderBy("TimeStamps", descending: false)
-                    .snapshots(),
+                stream: FirebaseFirestore.instance.collection("User Posts").orderBy("TimeStamps", descending: false).snapshots(),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
                     return ListView.builder(
-                      itemCount: snapshot.data!.docs.length, // Add itemCount
+                      itemCount: snapshot.data!.docs.length,
                       itemBuilder: (context, index) {
                         final post = snapshot.data!.docs[index];
-                        return WallPosts(
-                          message: post['Message'],
-                          user: post['User email'],
-                          postId: post.id,
-                          likes: List<String>.from(post['Likes'] ?? []),
-                          time: formatDate(post['TimeStamps']),
+                        final postId = post.id;
+                        final postMessage = post['Message'];
+                        final postUserEmail = post['User email'];
+
+                        return Dismissible(
+                          key: Key(postId),
+                          background: Container(
+                            color: Colors.green,
+                            alignment: Alignment.centerLeft,
+                            padding: const EdgeInsets.only(left: 20),
+                            child: const Icon(Icons.edit, color: Colors.white),
+                          ),
+                          secondaryBackground: Container(
+                            color: Colors.red,
+                            alignment: Alignment.centerRight,
+                            padding: const EdgeInsets.only(right: 20),
+                            child: const Icon(Icons.delete, color: Colors.white),
+                          ),
+                          confirmDismiss: (direction) async {
+                            if (direction == DismissDirection.startToEnd) {
+                              // Swipe right to edit
+                              if (postUserEmail == currentUserEmail) {
+                                editPost(context, postId, postMessage);
+                                return false; // Prevent dismissal after editing
+                              } else {
+                                Fluttertoast.showToast(msg: "You can only edit your own posts.");
+                                return false; // Do not dismiss if not post owner
+                              }
+                            } else if (direction == DismissDirection.endToStart) {
+                              // Swipe left to delete
+                              if (postUserEmail == currentUserEmail) {
+                                deletePost(postId);
+                                return true; // Allow dismissal after deletion
+                              } else {
+                                Fluttertoast.showToast(msg: "You cannot delete someone else's post.");
+                                return false; // Do not dismiss if not post owner
+                              }
+                            }
+                            return false;
+                          },
+                          child: WallPosts(
+                            message: postMessage,
+                            user: postUserEmail,
+                            postId: postId,
+                            likes: List<String>.from(post['Likes'] ?? []),
+                            time: formatDate(post['TimeStamps']),
+                          ),
                         );
                       },
                     );
@@ -74,7 +154,6 @@ class _CommunityPageState extends State<CommunityPage> {
                 },
               ),
             ),
-            // Remove the message input section here
           ],
         ),
       ),
