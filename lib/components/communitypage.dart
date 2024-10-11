fcdf0e5154a0dbc5ca43b36fb57adef746735181
madhawaawishka99 <madhawaawishka@gmail.com>
2024-10-11 18:01:48 +0530

    Update title text in CommunityPage

--- lib/components/communitypage.dart
+++ lib/components/communitypage.dart
@@ -139,7 +139,7 @@ class _CommunityPageState extends State<CommunityPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          "Explore Communitiesfdffdfdfdffdssgfgfgfgss",
+          "Explore Communitiesfdffdfdfdffdssss",
           style: TextStyle(
             color: Colors.black,
             fontWeight: FontWeight.bold,
